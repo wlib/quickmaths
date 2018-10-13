10284fe55aec71613810d57a48a9193183b207a5
Daniel Ethridge <danielethridge@icloud.com>
2018-10-13 15:58:19 -0400

    Use url query-string

--- src/js/state.js
+++ src/js/state.js
@@ -1,6 +1,19 @@
+function getQuerys() {
+  const query = {};
+  const pairs = document.location.search.substr(1).split("&");
+  pairs.forEach(pair => {
+    const [key, value] = pair.split("=");
+    query[decodeURIComponent(key)] = JSON.parse(
+      decodeURIComponent(value || "")
+    );
+  });
+  return query;
+}
+
+const queries = getQuerys();
+
 export default {
   correctAnswer: undefined,
-  type: undefined,
-  min: 1,
-  max: 50
+  type: queries.type,
+  range: queries.range
 };
